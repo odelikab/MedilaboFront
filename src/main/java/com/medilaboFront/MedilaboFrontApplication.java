6368d45b5946995571fccf5b10e1531581d26929
odeli <misterkb5@hotmail.com>
2024-02-20 00:46:04 +0100

    front setup

---
+++ src/main/java/com/medilaboFront/MedilaboFrontApplication.java
@@ -0,0 +1,15 @@
+package com.medilaboFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class MedilaboFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilaboFrontApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/medilaboFront/proxies/MicroserviceDiagProxy.java
@@ -0,0 +1,17 @@
+package com.medilaboFront.proxies;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "medilaboDiag", url = "localhost:8080")
+
+public interface MicroserviceDiagProxy {
+
+	@GetMapping("/diag/risque/{id}")
+	public String getRisqueById(@PathVariable("id") Integer id);
+
+	@GetMapping("/diag/risque/{name}")
+	public String getRisqueByName(@PathVariable("name") String name);
+
+}
